30cba3db5c458efbc9a1ef1afd23c56216bffc0f
rajitha-00 <rajithatw07102@gmail.com>
2023-05-04 17:07:07 +0100

    login done

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+/* eslint-disable */
+<template>
+    <div class="w-100 bg-pink-800 text-white">
+        <h4 class="text-center py-5">Sharmila S © 2021</h4>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+      name: 'Footer'
+  }
+  </script>
+  
+  <style>
+  </style>
